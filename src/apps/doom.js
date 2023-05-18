b655381768ca4e8fedab408cf53967f4bdac4812
Tiago <produtivo63@gmail.com>
2023-05-18 17:22:03 -0300

    raycasting do doom feito

--- src/apps/doom.js
+++ src/apps/doom.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useRef, useEffect} from 'react';
 
 const map = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 1, 0, 1, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-];
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 2, 2, 0, 0, 1],
+    [1, 0, 0, 2, 2, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+  ];
 
 const Screen = styled.div`
     position:fixed;
@@ -24,7 +25,7 @@ function distance(x1, y1, x2, y2) {
 }
 
 function renderMinimap(posX = 0, posY = 0, scale, rays,ctx){
-    const cellSize = scale * 16;
+    const cellSize = scale * 32;
     map.forEach((row, y) => {
         row.forEach((cell, x) => {
           if (cell) {
@@ -57,50 +58,51 @@ function outOfMapBounds(x, y) {
 
 function getVCollision(angle) {
     const right = Math.abs(Math.floor((angle - Math.PI / 2) / Math.PI) % 2);
-    const firstX = right ? Math.floor(player.x / 16) * 16 + 16 : Math.floor(player.x / 16) * 16;
+    const firstX = right ? Math.floor(player.x / 32) * 32 + 32 : Math.floor(player.x / 32) * 32;
     const firstY = player.y + (firstX - player.x) * Math.tan(angle);
-    const xA = right ? 16 : -16;
+    const xA = right ? 32 : -32;
     const yA = xA * Math.tan(angle);
     let wall;
     let nextX = firstX;
     let nextY = firstY;
+    let color = "#013aa6";
     while (!wall) {
-      const cellX = right ? Math.floor(nextX / 16) : Math.floor(nextX / 16) - 1;
-      const cellY = Math.floor(nextY / 16);
+      const cellX = right ? Math.floor(nextX / 32) : Math.floor(nextX / 32) - 1;
+      const cellY = Math.floor(nextY / 32);
       if (outOfMapBounds(cellX, cellY)) break;
       wall = map[cellY][cellX];
+      if (wall == 2) color = "#6bcdb3"; 
+      console.log(wall)
       if (!wall) {
         nextX += xA;
         nextY += yA;
       } 
     }
-    return {angle, distance: distance(player.x, player.y, nextX, nextY), vertical: true};
+    return {angle, distance: distance(player.x, player.y, nextX, nextY), vertical: true, color};
   }
   
   function getHCollision(angle) {
     const up = Math.abs(Math.floor(angle / Math.PI) % 2);
-    const firstY = up ? Math.floor(player.y / 16) * 16 : Math.floor(player.y / 16) * 16 + 16;
+    const firstY = up ? Math.floor(player.y / 32) * 32 : Math.floor(player.y / 32) * 32 + 32;
     const firstX = player.x + (firstY - player.y) / Math.tan(angle);
-  
-    const yA = up ? -16 : 16;
+    const yA = up ? -32 : 32;
     const xA = yA / Math.tan(angle);
-  
     let wall;
     let nextX = firstX;
     let nextY = firstY;
+    let color = "#012975";
     while (!wall) {
-      const cellX = Math.floor(nextX / 16);
-      const cellY = up ? Math.floor(nextY / 16) - 1 : Math.floor(nextY / 16);
-  
+      const cellX = Math.floor(nextX / 32);
+      const cellY = up ? Math.floor(nextY / 32) - 1 : Math.floor(nextY / 32);
       if (outOfMapBounds(cellX, cellY))  break;
-  
       wall = map[cellY][cellX];
+      if (wall == 2) color = "#00b0aa"; 
       if (!wall) {
         nextX += xA;
         nextY += yA;
       }
     }
-    return {angle, distance: distance(player.x, player.y, nextX, nextY), vertical: false};
+    return {angle, distance: distance(player.x, player.y, nextX, nextY), vertical: false, color};
 }
 
 function castRay(angle) {
@@ -111,7 +113,7 @@ function castRay(angle) {
 
 function getRays() {
     const initialAngle = player.angle - (60 * Math.PI) / 180 /2;
-    const numberOfRays = 400;
+    const numberOfRays = 500;
     const angleStep = ((60 * Math.PI) / 180) / numberOfRays;
     return Array.from({ length: numberOfRays }, (_, i) => {
       const angle = initialAngle + i * angleStep;
@@ -122,14 +124,14 @@ function getRays() {
 
 function renderScene(rays, ctx) {
     rays.forEach((ray, i) => {
-        const distance = ray.distance * Math.cos(ray.angle * player.angle)
-        const wallHeight = ((400 * 5) / distance) * 277;
-        ctx.fillStyle = ray.vertical ? "#012975" : "#013aa6";
-        ctx.fillRect(i, 400 / 2 - wallHeight / 2, 1, wallHeight);
+        const distance = ray.distance * Math.cos(ray.angle - player.angle)
+        const wallHeight = ((32 * 5) / distance) * 277;
+        ctx.fillStyle = ray.color;
+        ctx.fillRect(i, 500 / 2 - wallHeight / 2, 1, wallHeight);
         ctx.fillStyle = "#d52b1e";
-        ctx.fillRect(i,400 / 2 + wallHeight / 2,1,400 / 2 - wallHeight / 2);
-        ctx.fillStyle = "#ffffff";
-        ctx.fillRect(i, 0, 1, 400 / 2 - wallHeight / 2);
+        ctx.fillRect(i,500/2 + wallHeight / 2,1,500/2 - wallHeight / 2);
+        //ctx.fillStyle = "#ffffff";
+        //ctx.fillRect(i, 0, 1, 500/2 - wallHeight / 2);
     });
 }
 
@@ -140,22 +142,24 @@ function loop(doomRef){
     player.x += Math.cos(player.angle) * player.speed;
     player.y += Math.sin(player.angle) * player.speed;
     const rays = getRays();
-    //renderScene(rays);
+    renderScene(rays,ctx);
     renderMinimap(0, 0, 0.75, getRays(), ctx);
 }
 
-const player = {x: 16 * 1.5, y: 16 * 2, angle: 0 * Math.PI / 180, speed: 0};
+const player = {x: 32 * 1.5, y: 32 * 2, angle: 0 * Math.PI / 180, speed: 0};
 
 function Doom() { 
     const doomRef = useRef(null); 
 
     document.addEventListener("keydown", (e) => {
-        if (e.key === "ArrowUp") player.speed = 2;
-        if (e.key === "ArrowDown") player.speed = -2;
+        if (e.key === "ArrowUp" || e.key === "w") player.speed = 2;
+        if (e.key === "ArrowDown" || e.key === "s") player.speed = -2;
+        if (e.key === "ArrowRight" || e.key === "d") player.angle += 2 * Math.PI / 180; //player.y += 2;
+        if (e.key === "ArrowLeft" || e.key === "a") player.angle -= 2 * Math.PI / 180; //player.y -= 2
     });
       
     document.addEventListener("keyup", (e) => {
-        if (e.key === "ArrowUp" || e.key === "ArrowDown") player.speed = 0;
+        if (e.key === "ArrowDown" || e.key === "ArrowUp" || e.key === "w" || e.key === "s") player.speed = 0;
     });
       
     document.addEventListener("mousemove", function (e) {
@@ -164,7 +168,7 @@ function Doom() {
 
     useEffect(() => {setInterval(() => {loop(doomRef)}, 10)}, [player]);
     return (
-        <Screen><canvas ref={doomRef} height="400" width="400"/></Screen>
+        <Screen><canvas ref={doomRef} height="500" width="500"/></Screen>
     );
 }
 
